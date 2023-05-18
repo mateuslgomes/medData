3701ac9d16eba54b6ededecf181ad02a8dc76486
mateuslgomes <mateuslgomes.dev@gmail.com>
2023-05-18 14:44:16 -0300

    criando a classe de configuração do Spring Security

---
+++ api/src/main/java/med/data/api/infra/exception/security/config/SecurityConfigurations.java
@@ -0,0 +1,23 @@
+package med.data.api.infra.exception.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and();
+        return http.build();
+    }
+
+}
---
+++ api/src/main/java/med/data/api/infra/exception/security/controller/AutenticaController.java
@@ -0,0 +1,13 @@
+package med.data.api.infra.exception.security.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("login")
+public class AutenticaController {
+
+
+
+}
