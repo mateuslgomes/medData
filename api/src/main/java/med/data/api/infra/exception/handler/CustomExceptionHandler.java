7f2b3d29d92adb6163000ebff4acababee1a9b29
mateuslgomes <mateuslgomes.dev@gmail.com>
2023-05-18 13:50:58 -0300

    personalizando exception para cadastro de medico

--- api/src/main/java/med/data/api/infra/exception/handler/CustomExceptionHandler.java
+++ api/src/main/java/med/data/api/infra/exception/handler/CustomExceptionHandler.java
@@ -1,18 +1,32 @@
 package med.data.api.infra.exception.handler;
 
-import jakarta.persistence.EntityNotFoundException;
 import med.data.api.infra.exception.exceptions.MedicoNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(MedicoNotFoundException.class)
-    public ResponseEntity<String> tratarMedicoNotFoundException(MedicoNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ResponseEntity<String> tratarMedicoNotFoundException(MedicoNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErroValidacaoDto>> tratarMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        return ResponseEntity.badRequest().body(ex.getFieldErrors().stream().map(ErroValidacaoDto::new).toList());
+    }
+
+    private record ErroValidacaoDto(String campo, String mensagem){
+        public ErroValidacaoDto(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 
 }
