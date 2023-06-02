b6d3d6041b4f25c184f68904311e4ea97e5b970d
mateuslgomes <dev.mateuslgomes@gmail.com>
2023-06-02 13:06:54 -0300

    add validação para marcar a consulta com antecendêcia mínima de 30 minutos

---
+++ api/src/main/java/med/data/api/domain/consultas/validacoes/ValidadorHorarioAntecendencia.java
@@ -0,0 +1,20 @@
+package med.data.api.domain.consultas.validacoes;
+
+import med.data.api.domain.consultas.dtos.AgendamentoConsultaDto;
+import med.data.api.infra.exception.exceptions.ValidacaoException;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class ValidadorHorarioAntecendencia {
+
+    public void validar(AgendamentoConsultaDto dto) {
+        var dataConsulta = dto.data();
+        var agora = LocalDateTime.now();
+        var diferencaMinutos = Duration.between(agora, dataConsulta).toMinutes();
+        if (diferencaMinutos < 30) {
+            throw new ValidacaoException("A consulta deve ser agendada com antecendncia mínima de 30 minutos");
+        }
+    }
+
+}
