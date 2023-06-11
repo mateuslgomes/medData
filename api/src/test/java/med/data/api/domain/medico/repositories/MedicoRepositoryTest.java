87313b11d43f0bd65ee4d14007246e7cd05fb687
mateuslgomes <dev.mateuslgomes@gmail.com>
2023-06-10 21:14:43 -0300

    Configurando o banco de dados de testes

---
+++ api/src/test/java/med/data/api/domain/medico/repositories/MedicoRepositoryTest.java
@@ -0,0 +1,21 @@
+package med.data.api.domain.medico.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class MedicoRepositoryTest {
+
+    @Test
+    void buscarMedicoAleatorioLivreNaData() {
+
+    }
+
+}
